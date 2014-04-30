0b4bf11d0a8f141e4315b06a575bb9173706e0b5
kedarm <kedarm@xpanxion.co.in>
2014-04-30 18:28:08 +0530

    First commit

---
+++ GitTfs.Web/GitTfs.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GitTfs.Web.Startup))]
+namespace GitTfs.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
